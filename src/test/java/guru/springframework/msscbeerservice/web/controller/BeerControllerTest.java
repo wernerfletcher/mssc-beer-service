0e7c86dd22dd4ddd9380d72f6fa7965f523a0304
Werner Fletcher <wernerf@cibecs.com>
2021-02-22 20:45:38 +0200

    Fixed failing tests

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,14 @@ package guru.springframework.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -31,7 +33,7 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto newBeerDto = BeerDto.builder().build();
+        BeerDto newBeerDto = generateBeerDto();
         String newBeerDtoJson = objectMapper.writeValueAsString(newBeerDto);
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -42,12 +44,21 @@ class BeerControllerTest {
 
     @Test
     void updateBeer() throws Exception {
-        BeerDto updatedBeerDto = BeerDto.builder().build();
+        BeerDto updatedBeerDto = generateBeerDto();
         String updatedBeerDtoJson = objectMapper.writeValueAsString(updatedBeerDto);
 
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(updatedBeerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    private BeerDto generateBeerDto() {
+        return BeerDto.builder()
+                .beerName("Castle")
+                .beerStyle(BeerStyle.LAGER)
+                .upc(1234567890L)
+                .price(new BigDecimal("14.99"))
+                .build();
     }
 }
